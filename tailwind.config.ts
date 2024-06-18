d107f8c2255001d3cc49658a07a98a978db99846
Muhammad Selim <muhamadselim70@gmail.com>
2024-06-18 19:57:59 +0300

    chore: Add configuration files and dependencies for Next.js and Tailwind CSS

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      screens: {
+        xs: "375px",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)", "Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
